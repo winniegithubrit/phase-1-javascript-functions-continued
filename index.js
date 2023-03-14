1a01bd6de2469b8979bf9d7e2f9533c432179ec0
winniegithubrit <winnie.jomo@student.moringaschool.com>
2023-03-14 16:14:05 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+function saturdayFun( activity = 'roller-skate') {
+
+return(`This Saturday, I want to ${activity}!`);
+}
+
+function mondayWork( activity = 'go to the office') {
+      
+  return( `This Monday, I will ${activity}.`);
+
+}
+function wrapAdjective(str='*') {
+  return function (param='special'){
+return `You are ${str}${param}${str}!`
+  }
+
+}
